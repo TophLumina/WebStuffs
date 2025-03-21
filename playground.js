6306a3e6d45442224b60fdab28c2a3a42b8af56d
UncleRib <116142707+UncleRib@users.noreply.github.com>
2025-03-22 02:57:18 +1100

    update USD Buffer to 2

--- playground.js
+++ playground.js
@@ -1,6 +1,6 @@
 const apikey = "6b3e3f0df014da0075645a7d";
 const source = "AUD";
-const target_buffer_rate = { "CNY": 2, "USD": 1, "HKD": 1, "NZD": 1, "JPY": 1, "EUR": 1, "CAD": 1, "GBP": 1, "SGD": 1, "MYR": 1, "TWD": 1, "INR": 1 };
+const target_buffer_rate = { "CNY": 2, "USD": 2, "HKD": 1, "NZD": 1, "JPY": 1, "EUR": 1, "CAD": 1, "GBP": 1, "SGD": 1, "MYR": 1, "TWD": 1, "INR": 1 };
 var rate_sheet;
 
 // 创建一个显示下一次查询时间的容器
